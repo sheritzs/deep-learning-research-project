3996c8a2912bd0c0def212b46193f406aac3e092
Sheritz Sogbesan <ssogbesan@gmail.com>
2024-09-15 21:44:49 -0400

    add description and modify savefig argument

--- products/jupyter_notebooks/project_code/visualization_functions.py
+++ products/jupyter_notebooks/project_code/visualization_functions.py
@@ -4,12 +4,12 @@ import numpy as np
 import seaborn as sns
 
 
-def correlation_matrix(df, cmap='coolwarm', mask=True, name=None):
+def correlation_matrix(df, figsize=(18,6), cmap='coolwarm', mask=True, name=None):
     """Accepts a dataframe and generates a correlation matrix. If
     a name is provided, the image is saved."""
     corr = df.corr()
 
-    plt.subplots(figsize=(18, 8))
+    plt.subplots(figsize=figsize)
 
     # generate a mask for the upper triangle
     if mask:
@@ -23,6 +23,6 @@ def correlation_matrix(df, cmap='coolwarm', mask=True, name=None):
     plt.tight_layout()
 
     if name:
-        plt.savefig(name)
+        plt.savefig(f'{name}.png')
 
     plt.show()
