6a0be57407b8e2ff07cbfbf6389c236295d7701d
Sheritz Sogbesan <ssogbesan@gmail.com>
2024-10-09 21:51:21 -0400

    add pl pruning callback class

--- products/jupyter_notebooks/project_code/processing_functions.py
+++ products/jupyter_notebooks/project_code/processing_functions.py
@@ -9,6 +9,7 @@ import pandas as pd
 import re
 import time
 import urllib.request
+import warnings
 
 from darts.dataprocessing.transformers import Scaler
 from darts.models import (BlockRNNModel, ExponentialSmoothing, LightGBMModel, NBEATSModel,
@@ -691,3 +692,56 @@ def highlight_maxormin(df, max=True, starting_col_idx=0):
 
 
     return df_styled
+
+
+class PyTorchLightningPruningCallback(Callback):
+    """
+    PyTorch Lightning callback to prune unpromising trials
+    and address minor issue due to PyTorch-lighting default sanity check value.
+    source: https://github.com/optuna/optuna-examples/issues/166#issuecomment-1403112861
+
+    if you want to add a pruning callback which observes accuracy.
+    Args:
+        trial:
+            A :class:`~optuna.trial.Trial` corresponding to the current evaluation of the
+            objective function.
+        monitor:
+            An evaluation metric for pruning, e.g., ``val_loss`` or
+            ``val_acc``. The metrics are obtained from the returned dictionaries from e.g.
+            ``pytorch_lightning.LightningModule.training_step`` or
+            ``pytorch_lightning.LightningModule.validation_epoch_end`` and the names thus depend on
+            how this dictionary is formatted.
+    """
+
+    def __init__(self, trial: optuna.trial.Trial, monitor: str) -> None:
+        super().__init__()
+
+        self._trial = trial
+        self.monitor = monitor
+
+    def on_validation_end(self, trainer: Trainer, pl_module: LightningModule) -> None:
+        # When the trainer calls `on_validation_end` for sanity check,
+        # do not call `trial.report` to avoid calling `trial.report` multiple times
+        # at epoch 0. The related page is
+        # https://github.com/PyTorchLightning/pytorch-lightning/issues/1391.
+        if trainer.sanity_checking:
+            return
+
+        epoch = pl_module.current_epoch
+
+        current_score = trainer.callback_metrics.get(self.monitor)
+        if current_score is None:
+            message = (
+                "The metric '{}' is not in the evaluation logs for pruning. "
+                "Please make sure you set the correct metric name.".format(self.monitor)
+            )
+            warnings.warn(message)
+            return
+
+        self._trial.report(current_score, step=epoch)
+        if self._trial.should_prune():
+            message = "Trial was pruned at epoch {}.".format(epoch)
+            raise optuna.TrialPruned(message)
+
+
+
